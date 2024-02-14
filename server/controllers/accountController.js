56bca6e9b010cbeac6dfa48be9b8c43a85691665
Tejas Raghuram <tejas.raghuram101@gmail.com>
2024-02-14 16:43:14 -0500

    Implemented Account Creation API

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -1,7 +1,34 @@
 const User = require('../models/User');
 
 const create = async (req, res) => {
-    
+    const {
+        username, 
+        password
+    } = req.body;
+    const leagues = [];
+
+    try {
+        const exists = await User.findOne({'username': username});
+        if(exists)
+        {
+            res.status(400).json({error: "Account Already Exists"});
+        }
+        else if(username.length === 0 || password.length === 0 || !username.match('/^[a-z0-9]+$/i') || !password.match('/^[a-z0-9]+$/i'))
+        {
+            res.status(400).json({error: "Invalid Username or Password"});
+        }
+        else
+        {
+            const account = await User.create({
+                username, 
+                password, 
+                leagues
+            });
+            res.status(200).json(account);
+        }
+    } catch(error) {
+        res.status(400).json({error: error.message});
+    }
 }
 
 const verify = (req, res) => {
