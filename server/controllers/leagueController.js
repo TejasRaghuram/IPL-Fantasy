eda5c53fabacd5a01ce2462a96bd43ffeee173fd
Tejas Raghuram <tejas.raghuram101@gmail.com>
2024-02-19 19:14:37 -0500

    Implemented Squad Retrieval API

--- server/controllers/leagueController.js
+++ server/controllers/leagueController.js
@@ -21,7 +21,24 @@ const users = async (req, res) => {
 };
 
 const squad = async (req, res) => {
+    const {
+        username,
+        league
+    } = req.body;
 
+    try {
+        const squad = await Squad.find({'username': username, 'league': league});
+        if(!squad)
+        {
+            res.status(400).json({error: 'User or League Does Not Exist'});
+        }
+        else
+        {
+            res.status(200).json(squad);
+        }
+    } catch(error) {
+        res.status(400).json({error: error.message});
+    }
 };
 
 module.exports = {
