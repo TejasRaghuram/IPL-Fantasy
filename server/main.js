0f5666722054340686e6483a16b81a7924c826d4
Tejas Raghuram <tejas.raghuram101@gmail.com>
2024-02-20 17:45:28 -0500

    Added CORS to API

--- server/main.js
+++ server/main.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const accountRoutes = require('./routes/account');
 const adminRoutes = require('./routes/admin');
@@ -10,6 +11,8 @@ const userRoutes = require('./routes/user');
 const leagueRoutes = require('./routes/league');
 
 const server = express();
+
+server.use(cors());
 
 server.use(express.json());
 
