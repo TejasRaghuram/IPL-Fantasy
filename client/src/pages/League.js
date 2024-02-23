0466534a790a2c955c36b997cf37a23cdf0c32e1
Tejas Raghuram <tejas.raghuram101@gmail.com>
2024-02-22 22:43:58 -0500

    Implemented User List Front End

--- client/src/pages/League.js
+++ client/src/pages/League.js
@@ -1,4 +1,5 @@
 import { useNavigate, useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './../styles/League.css';
 
 function User(props) {
@@ -20,15 +21,48 @@ function User(props) {
 
 function League() {
     const params = useParams();
+    const navigate = useNavigate();
+    const [content, setContent] = useState(<p>Loading...</p>);
+
+    useEffect(() => {
+        const league = params.league;
+        fetch('/api/league/users', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({league})
+        })
+            .then(response => response.json())
+            .then(json => {
+                try {
+                    const rank = (u1, u2) => {
+                        return u2.points - u1.points;
+                    };
+                    json.sort(rank);
+                    const rankings = [];
+                    let currentRank = 1;
+                    let currentPoints = json[0].points;
+                    for(let i = 0; i < json.length; i++)
+                    {
+                        if(json[i].points < currentPoints)
+                        {
+                            currentRank++;
+                            currentPoints = json[i].points;
+                        }
+                        rankings.push(<User rank={currentRank} username={json[i].username} points={json[i].points}/>);
+                    }
+                    setContent(rankings);
+                } catch(error) {
+                    navigate('/');
+                }
+            });
+    }, [params.league, navigate]);
 
     // make sure league exists
 
     return(
         <div id='league-content'>
             <h1>{params.league}</h1>
-            <User username={'TejasRaghuram'} points={20349}/>
-            <User username={'IshaanSinha'} points={19872}/>
-            <User username={'VigneshSaravanakumar'} points={19428}/>
+            {content}
         </div>
     );
 }
