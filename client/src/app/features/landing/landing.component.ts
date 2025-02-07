e2e022efa2c89f5a2fe89cb1270be313807d8375
TejasRaghuram <tejas.raghuram101@gmail.com>
2025-02-06 19:27:51 -0500

    Created and Styled Landing Page

--- client/src/app/features/landing/landing.component.ts
+++ client/src/app/features/landing/landing.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
-export class LandingComponent {
+export class LandingComponent implements AfterViewInit {
+  constructor(private elementRef : ElementRef) {}
 
+  ngAfterViewInit(): void {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#FFE6BE';
+    this.elementRef.nativeElement.ownerDocument.body.style.overflow="hidden";
+  }
 }
