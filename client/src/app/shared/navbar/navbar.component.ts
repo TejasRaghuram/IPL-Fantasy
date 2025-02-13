88a79879db6ee77f187f216937960cb98d321f7f
TejasRaghuram <tejas.raghuram101@gmail.com>
2025-02-12 19:48:59 -0500

    Created and Styled Navigation Bar

--- client/src/app/shared/navbar/navbar.component.ts
+++ client/src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,25 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router: Router) {}
 
+  handleHome() {
+    this.router.navigate(["/home"]);
+  }
+
+  handlePlayerRankings() {
+    this.router.navigate(["/rankings"]);
+  }
+
+  handleLogOut() {
+    this.router.navigate(["/"]);
+  }
 }
