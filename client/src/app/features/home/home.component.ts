ccc6a95eb06a1cb5bdb1a08341aa7a88a1ea918f
TejasRaghuram <tejas.raghuram101@gmail.com>
2025-02-16 21:15:23 -0800

    Created and Styled Join Page

--- client/src/app/features/home/home.component.ts
+++ client/src/app/features/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +10,14 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   name = "Name";
   selection = "League 0";
+
+  constructor(private router: Router) {}
+
+  handleJoinLeague(): void {
+    this.router.navigate(["/join"]);
+  }
+
+  handleCreateLeague(): void {
+    this.router.navigate(["/create"]);
+  }
 }
--- client/src/app/features/join/join.component.ts
+++ client/src/app/features/join/join.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-join',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './join.component.html',
   styleUrl: './join.component.css'
 })
 export class JoinComponent {
+  leagueName = '';
+  players: string[] = [];
 
+  constructor(private router: Router) {}
+
+  handleVerify(name: string, password: string): void {
+    this.leagueName = name;
+    let numPlayers = 5;
+    this.players = Array(numPlayers).fill('');
+  }
+
+  handleJoin(): void {
+    alert(this.players);
+    this.router.navigate(["/home"]);
+  }
 }
