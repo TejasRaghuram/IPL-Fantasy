25ea9490a95f831f804440b8b48dfbd58caf6114
TejasRaghuram <tejas.raghuram101@gmail.com>
2025-02-19 20:44:26 -0800

    Created and Styled Player Page

--- client/src/app/features/player/player.component.ts
+++ client/src/app/features/player/player.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-player',
@@ -6,6 +7,17 @@ import { Component } from '@angular/core';
   templateUrl: './player.component.html',
   styleUrl: './player.component.css'
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit {
+  name = ''
+;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.name = params.get('name') || '';
+    });
+  }
+  invalidImage(event: any): void {
+    event.target.src = "https://scores.iplt20.com/ipl/images/default-player-statsImage.png?v=4";
+  }
 }
