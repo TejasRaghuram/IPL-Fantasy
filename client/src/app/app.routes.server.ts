a2ea6b037b360188c3411538ad595ec2a2b6be0c
TejasRaghuram <tejas.raghuram101@gmail.com>
2025-03-13 20:50:33 -0700

    Modified Page Render Modes

--- client/src/app/app.routes.server.ts
+++ client/src/app/app.routes.server.ts
@@ -2,6 +2,46 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'signup',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'rankings',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'create',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'join',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'error',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: ':name',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: ':league/:username',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
